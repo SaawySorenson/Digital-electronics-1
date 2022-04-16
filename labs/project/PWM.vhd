ce41c03f26ed3d5aecde47e0e6fcb7d6fbcfbf45
SaawySorenson <99694504+SaawySorenson@users.noreply.github.com>
2022-04-16 14:21:04 +0200

    Update PWM.vhd

--- labs/project/PWM.vhd
+++ labs/project/PWM.vhd
@@ -1,81 +1,47 @@
 ------------------------------------------------------------
---
--- Traffic light controller using FSM.
--- Nexys A7-50T, Vivado v2020.1.1, EDA Playground
---
--- Copyright (c) 2020-Present Tomas Fryza
--- Dept. of Radio Electronics, Brno Univ. of Technology, Czechia
--- This work is licensed under the terms of the MIT license.
---
--- This code is inspired by:
--- [1] LBEbooks, Lesson 92 - Example 62: Traffic Light Controller
---     https://www.youtube.com/watch?v=6_Rotnw1hFM
--- [2] David Williams, Implementing a Finite State Machine in VHDL
---     https://www.allaboutcircuits.com/technical-articles/implementing-a-finite-state-machine-in-vhdl/
--- [3] VHDLwhiz, One-process vs two-process vs three-process state machine
---     https://vhdlwhiz.com/n-process-state-machine/
---
-------------------------------------------------------------
 
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 ------------------------------------------------------------
--- Entity declaration for traffic light controller
+-- Entity declaration for sounds generation
 ------------------------------------------------------------
-entity tlc is
+entity sound_gen is
     port(
         clk     : in  std_logic;
         reset   : in  std_logic;
-        -- Traffic lights (RGB LEDs) for two directions
-        south_o : out std_logic_vector(3 - 1 downto 0);
-        west_o  : out std_logic_vector(3 - 1 downto 0)
+        
+        sound_o : out std_logic;
+        snd1_i  : int std_logic;
+        snd2_i  : int std_logic;
+        snd3_i  : int std_logic;
+        snd4_i  : int std_logic;
+        snd5_i  : int std_logic;
     );
-end entity tlc;
+end entity sound_gen;
 
 ------------------------------------------------------------
 -- Architecture declaration for traffic light controller
 ------------------------------------------------------------
-architecture Behavioral of tlc is
-
-    -- Define the states
-    type t_state is (STOP1,
-                     WEST_GO,
-                     WEST_WAIT,
-                     STOP2,
-                     SOUTH_GO,
-                     SOUTH_WAIT);
-    -- Define the signal that uses different states
-    signal s_state : t_state;
-
-    -- Internal clock enable
+architecture Behavioral of sound_gen is
+    
     signal s_en : std_logic;
+    signal s_snd : std_logic;
 
-    -- Local delay counter
-    signal s_cnt : unsigned(4 downto 0);
-
-    -- Specific values for local counter
-    constant c_DELAY_4SEC : unsigned(4 downto 0) := b"1_0000";
-    constant c_DELAY_2SEC : unsigned(4 downto 0) := b"0_1000";
-    constant c_DELAY_1SEC : unsigned(4 downto 0) := b"0_0100";
-    constant c_ZERO       : unsigned(4 downto 0) := b"0_0000";
-
-    -- Output values
-    constant c_RED        : std_logic_vector(2 downto 0) := b"100";
-    constant c_YELLOW     : std_logic_vector(2 downto 0) := b"110";
-    constant c_GREEN      : std_logic_vector(2 downto 0) := b"010";
-
-begin
+    constant c_ZERO       : unsigned(1 downto 0) := b"0_0";
+    constant c_ONE        : unsigned(1 downto 0) := b"0_1";
+    
+    begin
 
     --------------------------------------------------------
     -- Instance (copy) of clock_enable entity generates 
     -- an enable pulse every 250 ms (4 Hz). Remember that 
     -- the frequency of the clock signal is 100 MHz.
+        
     clk_en0 : entity work.clock_enable
         generic map(
-            g_MAX => 10000000 -- FOR SIMULATION PURPOSE ONLY !!!
-            -- FOR IMPLEMENTATION: g_MAX = 250 ms / (1/100 MHz)
+            g_MAX => 100000 -- test and finish here!!
         )
         port map(
             clk   => clk,
@@ -83,104 +49,23 @@ begin
             ce_o  => s_en
         );
 
-    --------------------------------------------------------
-    -- p_traffic_fsm:
-    -- The sequential process with synchronous reset and 
-    -- clock_enable entirely controls the s_state signal by 
-    -- CASE statement.
-    --------------------------------------------------------
-    p_traffic_fsm : process(clk)
+    
+    p_sound_gen : process(clk)
     begin
         if rising_edge(clk) then
             if (reset = '1') then   -- Synchronous reset
-                s_state <= STOP1;   -- Set initial state
-                s_cnt   <= c_ZERO;  -- Clear delay counter
+                s_snd <= c_ZERO;   -- Set initial state
+                
 
             elsif (s_en = '1') then
                 -- Every 250 ms, CASE checks the value of the s_state 
                 -- variable and changes to the next state according 
                 -- to the delay value.
-                case s_state is
-
-                    -- If the current state is STOP1, then wait 1 sec
-                    -- and move to the next GO_WAIT state.
-                    when STOP1 =>
-                        -- Count up to c_DELAY_1SEC
-                        if (s_cnt < c_DELAY_1SEC) then
-                            s_cnt <= s_cnt + 1;
-                        else
-                            -- Move to the next state
-                            s_state <= WEST_GO;
-                            -- Reset local counter value
-                            s_cnt <= c_ZERO;
-                        end if;
-
-                    when WEST_GO =>
-                        
-                        if (s_cnt < c_DELAY_4SEC) then
-                            s_cnt <= s_cnt + 1;
-                        else
-                            -- Move to the next state
-                            s_state <= WEST_WAIT;
-                            -- Reset local counter value
-                            s_cnt <= c_ZERO;
-                        end if;
-                        
-                    when WEST_WAIT =>
-                        
-                        if (s_cnt < c_DELAY_2SEC) then
-                            s_cnt <= s_cnt + 1;
-                        else
-                            -- Move to the next state
-                            s_state <= STOP2;
-                            -- Reset local counter value
-                            s_cnt <= c_ZERO;
-                        end if;
-                        
-                     when STOP2 =>                      
-                       
-                        if (s_cnt < c_DELAY_1SEC) then
-                            s_cnt <= s_cnt + 1;
-                        else
-                            -- Move to the next state
-                            s_state <= SOUTH_GO;
-                            -- Reset local counter value
-                            s_cnt <= c_ZERO;
-                        end if;
-                        
-                        when SOUTH_GO =>
-                       
-                       
-                        if (s_cnt < c_DELAY_4SEC) then
-                            s_cnt <= s_cnt + 1;
-                        else
-                            -- Move to the next state
-                            s_state <= SOUTH_WAIT;
-                            -- Reset local counter value
-                            s_cnt <= c_ZERO;
-                        end if;
-                        
-                        when SOUTH_WAIT =>
-                       
-                        
-                        if (s_cnt < c_DELAY_2SEC) then
-                            s_cnt <= s_cnt + 1;
-                        else
-                            -- Move to the next state
-                            s_state <= STOP1;
-                            -- Reset local counter value
-                            s_cnt <= c_ZERO;
-                        end if;
-                    -- It is a good programming practice to use the 
-                    -- OTHERS clause, even if all CASE choices have 
-                    -- been made.
-                    when others =>
-                        s_state <= STOP1;
-                        s_cnt   <= c_ZERO;
-                end case;
+               
+                
             end if; -- Synchronous reset
         end if; -- Rising edge
-    end process p_traffic_fsm;
+    end process p_sound_gen;
 
     --------------------------------------------------------
     -- p_output_fsm:
